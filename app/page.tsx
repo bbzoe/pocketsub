551344abc8114e5786ded5306033cc4785941e38
ss <sss>
2023-09-20 20:49:41 +0200

    Add small test

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='mx-4 text-center px-0'>
-      Hello from Pockethub
+      Hello from Pockethub (deploy)
     </main>
   )
 }
